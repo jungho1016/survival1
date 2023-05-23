b48ee3dc8cc8c7574d1811a336c920a17431069e
Jungho <jungho1016@gmail.com>
2023-05-23 16:34:51 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/survival1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.survival1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('h2'),
+        ),
+      ),
+    );
+  }
+}
